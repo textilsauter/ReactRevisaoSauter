829cb8a44e0f2a7887ab19691066094d3abef751
Edilio NUNES DUARTE JUNIOR <84047076+ediliojr@users.noreply.github.com>
2023-10-12 13:04:44 -0300

    dash

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -11,7 +11,7 @@ export class Home extends Component {
                 <img src="https://textilsauter.com.br/wp-content/uploads/2019/08/LOFGOWDWJKS.png" width="50%" height= "50%" ></img>
           
 
-                 <h4><a href='https://docs.google.com/spreadsheets/d/1JhCyb9RwVokmu7_5Z8VxGLfluYjZylOq/edit?usp=sharing&ouid=100848451469618663317&rtpof=true&sd=true'>DASHBOARD</a></h4>
+                 <h4><a href='https://docs.google.com/spreadsheets/d/1Fp-22Rvb1TOqRWwYTP2XHU81n5dzFQy7-l9_aiemuDU/edit?usp=sharing'>DASHBOARD</a></h4>
             </div>
         )
     }
